5222e69a48d63b936a9d03ca746ec51b52d55a75
JadeKim042386 <jk042386@gmail.com>
2023-08-07 20:37:25 +0900

    feat: 도메인 설계 - 공통 필드인 생성일시, 수정일시, 생성자, 수정자는 `JpaAuditing`을 사용 - `@OneToMany`, `@ManyToOne` 관계 추가 - `equals`, `hashcode` 메소드 추가
    * Article
    * ArticleComment
    * Like
    * UserAccount

---
+++ src/main/java/joo/project/my3d/domain/Article.java
@@ -0,0 +1,83 @@
+package joo.project.my3d.domain;
+
+import joo.project.my3d.domain.constant.ArticleCategory;
+import joo.project.my3d.domain.constant.ArticleType;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString(callSuper = true)
+@Table(
+        name = "article",
+        indexes = {
+                @Index(columnList = "article_id")
+        }
+)
+@Entity
+public class Article extends AuditingFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private UserAccount userAccount;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ArticleType articleType;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private ArticleCategory articleCategory; //"ArticleType=MODEL"일 경우 non-null
+
+    @ToString.Exclude
+    @OrderBy("createdAt DESC")
+    @OneToMany(mappedBy = "article")
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "article")
+    private final Set<Like> likes = new LinkedHashSet<>();
+
+    protected Article() {
+    }
+
+    private Article(UserAccount userAccount, String title, String content, ArticleType articleType, ArticleCategory articleCategory) {
+        this.userAccount = userAccount;
+        this.title = title;
+        this.content = content;
+        this.articleType = articleType;
+        this.articleCategory = articleCategory;
+    }
+
+    public static Article of(UserAccount userAccount, String title, String content, ArticleType articleType, ArticleCategory articleCategory) {
+        return new Article(userAccount, title, content, articleType, articleCategory);
+    }
+
+    public static Article of(UserAccount userAccount, String title, String content, ArticleType articleType) {
+        return Article.of(userAccount, title, content, articleType, null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article that)) return false;
+        return this.getId() != null && Objects.equals(this.getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
+}
---
+++ src/main/java/joo/project/my3d/domain/constant/ArticleType.java
@@ -0,0 +1,6 @@
+package joo.project.my3d.domain.constant;
+
+public enum ArticleType {
+    REQUEST_MODELING,
+    MODEL
+}
