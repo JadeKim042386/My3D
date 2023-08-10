1fac3f5389f69323f8b8dde40f0d2a7a8a9fdb03
JadeKim042386 <jk042386@gmail.com>
2023-08-10 11:22:58 +0900

    feat: ArticleFile 키 추가 및 sql 수정 - ArticleFile의 file_name은 unique해야하므로 unique key로 지정 - MariaDB에 적용되는 sql 수정 - 테스트 데이터로 사용되는 sql 수정 #10

--- database/initdb.d/create_table.sql
+++ database/initdb.d/create_table.sql
@@ -4,7 +4,7 @@
 	`user_password`	varchar(255)	NOT NULL,
 	`nickname`	varchar(100)	NOT NULL,
 	`user_role` varchar(10) NOT NULL,
-	`created_at`	datetime	NOT NULL,
+	`created_at`	datetime	DEFAULT CURRENT_TIMESTAMP   NOT NULL,
 	`modified_at`	datetime	NOT NULL,
 	`created_by`	varchar(100)	NOT NULL,
 	`modified_by`	varchar(100)	NOT NULL,
@@ -13,26 +13,27 @@
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='유저';
 
 CREATE TABLE `article_file` (
-    `id`	bigint	NOT NULL,
+    `id`	bigint	AUTO_INCREMENT NOT NULL,
     `byte_size`	bigint	NOT NULL,
     `file_name`	varchar(100)	NOT NULL,
     `file_extension`	varchar(100)	NOT NULL,
-    `created_at`	datetime	NOT NULL,
+    `created_at`	datetime	DEFAULT CURRENT_TIMESTAMP   NOT NULL,
     `modified_at`	datetime	NOT NULL,
     `created_by`	varchar(100)	NOT NULL,
     `modified_by`	varchar(100)	NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE (`file_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='파일';
 
 CREATE TABLE `article` (
-	`id`	bigint	NOT NULL,
+	`id`	bigint	AUTO_INCREMENT NOT NULL,
 	`user_id`   varchar(50)	NOT NULL,
     `article_file_id`   bigint  NOT NULL,
 	`title`	varchar(255)	NOT NULL,
 	`content`	varchar(255)	NOT NULL,
 	`article_type`	varchar(50)	NOT NULL,
 	`article_category`	varchar(50)	NULL,
-	`created_at`	datetime	NOT NULL,
+	`created_at`	datetime	DEFAULT CURRENT_TIMESTAMP   NOT NULL,
 	`modified_at`	datetime	NOT NULL,
 	`created_by`	varchar(100)	NOT NULL,
 	`modified_by`	varchar(100)	NOT NULL,
@@ -42,12 +43,12 @@ CREATE TABLE `article` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='게시글';
 
 CREATE TABLE `article_comment` (
-	`id`	bigint	NOT NULL,
+	`id`	bigint	AUTO_INCREMENT NOT NULL,
     `user_id`   varchar(50)	NOT NULL,
 	`article_id`	bigint	NOT NULL,
 	`content`	varchar(255)	NOT NULL,
 	`parent_comment_id`	bigint	NULL,
-	`created_at`	datetime	NOT NULL,
+	`created_at`	datetime	DEFAULT CURRENT_TIMESTAMP   NOT NULL,
 	`modified_at`	datetime	NOT NULL,
 	`created_by`	varchar(100)	NOT NULL,
 	`modified_by`	varchar(100)	NOT NULL,
@@ -57,10 +58,10 @@ CREATE TABLE `article_comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='댓글';
 
 CREATE TABLE `article_like` (
-	`id`	bigint	NOT NULL,
+	`id`	bigint	AUTO_INCREMENT NOT NULL,
 	`article_id`	bigint	NOT NULL,
 	`user_id`	varchar(50)	NOT NULL,
-	`created_at`	datetime	NOT NULL,
+	`created_at`	datetime	DEFAULT CURRENT_TIMESTAMP   NOT NULL,
 	`modified_at`	datetime	NOT NULL,
 	`created_by`	varchar(100)	NOT NULL,
 	`modified_by`	varchar(100)	NOT NULL,
