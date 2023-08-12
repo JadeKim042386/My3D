7135de9dd5e13b73724dd692deb7f797fe8dbdb5
JadeKim042386 <jk042386@gmail.com>
2023-08-12 23:48:05 +0900

    feat: `SecurityConfig` 추가 및 DTO 수정/추가 - `SecurityFilterChain`에서 인증과 role에 따른 엔드포인트 접근 권한을 설정하고 `Login`은 default로 설정 - `UserDetailsService`를 추가하고 `loadUserByUsername` 메소드를 간단히 람다식으로 구현 - `authentication`에 사용되는 `Principal` 객체를 DTO(`BoardPrincipal`)로 추가 - `UserAccountDto`는 누락된 필드를 추가하고 `BoardPrincipal`로 변환할 수 있도록 생성자 메소드를 추가  #10

--- src/main/java/joo/project/my3d/domain/constant/UserRole.java
+++ src/main/java/joo/project/my3d/domain/constant/UserRole.java
@@ -1,7 +1,16 @@
 package joo.project.my3d.domain.constant;
 
+import lombok.Getter;
+
 public enum UserRole {
-    USER,
-    ADMIN,
-    COMPANY
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN"),
+    COMPANY("ROLE_COMPANY");
+
+    @Getter
+    private final String name;
+
+    UserRole(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/joo/project/my3d/dto/UserAccountDto.java
+++ src/main/java/joo/project/my3d/dto/UserAccountDto.java
@@ -3,15 +3,29 @@ package joo.project.my3d.dto;
 import joo.project.my3d.domain.UserAccount;
 import joo.project.my3d.domain.constant.UserRole;
 
+import java.time.LocalDateTime;
+
 public record UserAccountDto(
         String userId,
         String userPassword,
         String email,
         String nickname,
-        UserRole userRole
+        UserRole userRole,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
 ) {
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, UserRole userRole, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, userPassword, email, nickname, userRole, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
     public static UserAccountDto of(String userId, String userPassword, String email, String nickname, UserRole userRole) {
-        return new UserAccountDto(userId, userPassword, email, nickname, userRole);
+        return new UserAccountDto(userId, userPassword, email, nickname, userRole, null, null, null, null);
+    }
+
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname) {
+        return new UserAccountDto(userId, userPassword, email, nickname, null, null, null, null, null);
     }
 
     public static UserAccountDto from(UserAccount userAccount) {
