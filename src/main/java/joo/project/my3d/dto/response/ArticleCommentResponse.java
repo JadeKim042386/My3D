9d6f5ec0835036ea3782d6c9785e2f0df697bfd1
JadeKim042386 <jk042386@gmail.com>
2023-08-16 17:07:01 +0900

    feat: 댓글관련 Response DTO 수정 - 댓글의 생성일시는 게시글의 생성일시와 동일한 포맷으로 수정 - 부모 댓글 id와 자식 댓글 set 필드 추가 - 댓글을 포함하는 게시글 DTO에서는 부모 댓글과 자식 댓글을 나누어 부모 댓글 안에 자식 댓글을 삽입하는 메소드를 추가 #10

--- src/main/java/joo/project/my3d/dto/response/ArticleCommentResponse.java
+++ src/main/java/joo/project/my3d/dto/response/ArticleCommentResponse.java
@@ -2,26 +2,46 @@ package joo.project.my3d.dto.response;
 
 import joo.project.my3d.dto.ArticleCommentDto;
 import joo.project.my3d.dto.UserAccountDto;
+import joo.project.my3d.utils.LocalDateTimeUtils;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
         String content,
+        String createdAt,
         String nickname,
-        LocalDateTime createdAt
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
 ) {
-    public static ArticleCommentResponse of(Long id, String content, String nickname, LocalDateTime createdAt) {
-        return new ArticleCommentResponse(id, content, nickname, createdAt);
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String nickname, String userId) {
+        return ArticleCommentResponse.of(id, content, createdAt, nickname, userId, null);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String nickname, String userId, Long parentCommentId) {
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(id, content, LocalDateTimeUtils.passedTime(createdAt), nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
         return ArticleCommentResponse.of(
                 dto.id(),
                 dto.content(),
+                dto.createdAt(),
                 dto.userAccountDto().nickname(),
-                dto.createdAt()
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
         );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
     }
 }
