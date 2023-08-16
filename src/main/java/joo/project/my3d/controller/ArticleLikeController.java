26fac42bfe3f4d47e330ce07b12de9db655bb923
JadeKim042386 <jk042386@gmail.com>
2023-08-16 11:14:05 +0900

    feat: 게시글 좋아요 컨트롤러 구현 - GET 메소드로 좋아요 추가/삭제를 하고 게시글 페이지로 redirect - 게시글을 인증된 유저가 좋아요를 눌렀는지 확인해야하기때문에 게시글 페이지에도 권한 추가 - 좋아요 추가/삭제는 인증된 유저가 할 수 있도록 `SecurityConfig` 수정  #10

---
+++ src/main/java/joo/project/my3d/controller/ArticleLikeController.java
@@ -0,0 +1,37 @@
+package joo.project.my3d.controller;
+
+import joo.project.my3d.dto.security.BoardPrincipal;
+import joo.project.my3d.service.ArticleLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/like")
+@RequiredArgsConstructor
+public class ArticleLikeController {
+
+    private final ArticleLikeService articleLikeService;
+
+    @GetMapping("{articleId}")
+    public String addArticleLike(
+            @PathVariable Long articleId,
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal
+    ) {
+        articleLikeService.addArticleLike(articleId, boardPrincipal.username());
+
+        return "redirect:/model_articles/" + articleId;
+    }
+
+    @GetMapping("{articleId}/delete")
+    public String deleteArticleLike(
+            @PathVariable Long articleId
+    ) {
+        articleLikeService.deleteArticleLike(articleId);
+
+        return "redirect:/model_articles/" + articleId;
+    }
+}
