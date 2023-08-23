a10ac7068b616fe8d30e38454c8fc3d1ac27ae1f
JadeKim042386 <jk042386@gmail.com>
2023-08-23 19:11:40 +0900

    feat: CustomOAuth2LoginAuthenticationFilter 수정 - 로그인과 회원가입, 이메일 인증 코드 발송 관련 URL 요청에 대해서만 회원가입 여부에 따라 로직을 수행하고 이외의 URL에서는 로그인과 회원가입에서 사용되는 모든 세션 attribute 값들을 제거 #27

--- src/main/java/joo/project/my3d/config/CustomOAuth2LoginAuthenticationFilter.java
+++ src/main/java/joo/project/my3d/config/CustomOAuth2LoginAuthenticationFilter.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -19,15 +20,35 @@ public class CustomOAuth2LoginAuthenticationFilter extends OncePerRequestFilter 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!Objects.isNull(authentication)) {
-            BoardPrincipal boardPrincipal = (BoardPrincipal) authentication.getPrincipal();
-            //회원가입이 되지 않은 사용자는 인증 실패
-            if (boardPrincipal.notSignUp()) {
-                HttpSession session = request.getSession();
-                session.setAttribute("email", boardPrincipal.email());
-                session.setAttribute("nickname", boardPrincipal.nickname());
-                SecurityContextHolder.clearContext();
+        String url = String.valueOf(request.getRequestURL());
+        //로그인과 회원가입 관련 URL에서만 실행
+        if (url.contains("account") || url.contains("/mail/send_code")) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (!Objects.isNull(authentication)) {
+                BoardPrincipal boardPrincipal = (BoardPrincipal) authentication.getPrincipal();
+                //회원가입이 되지 않은 사용자는 인증 실패
+                if (boardPrincipal.notSignUp()) {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("email", boardPrincipal.email());
+                    session.setAttribute("nickname", boardPrincipal.nickname());
+                    session.setAttribute("oauthLogin", true);
+                    SecurityContextHolder.clearContext();
+                }
+            }
+        } else {
+            //로그인과 회원가입에서 사용되는 모든 세션 attribute 삭제
+            List<String> sessionAttrs = List.of(
+                    "b_no",
+                    "b_stt_cd",
+                    "email",
+                    "nickname",
+                    "emailCode",
+                    "userRole",
+                    "duplicatedEmail"
+            );
+            HttpSession session = request.getSession();
+            for (String sessionAttr : sessionAttrs) {
+                session.removeAttribute(sessionAttr);
             }
         }
 
