93917235d277d6085dd51d8daa8db90be62b3de6
JadeKim042386 <jk042386@gmail.com>
2023-08-26 17:25:13 +0900

    feat: Jwt 토큰 처리를 위한 security 설정과 추가 -  OAuth 로그인 완료 이후 Jwt 토큰을 생성 후 쿠키로 저장하도록 successHandler 추가 - 쿠키에서 Jwt 토큰을 가져와 authentication을 등록하기위한 필터(JwtTokenFilter) 추가 - Jwt 토큰을 사용할 경우 stateless한 특성때문에 session을 사용하지않아 sessionManagement에 stateless를 설정해야하지만 LoginController에서 아직 session을 사용하기때문에 이후 리팩토링 후 설정할 예정 #34

--- src/main/java/joo/project/my3d/config/SecurityConfig.java
+++ src/main/java/joo/project/my3d/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package joo.project.my3d.config;
 
+import joo.project.my3d.config.filter.CustomOAuth2LoginAuthenticationFilter;
+import joo.project.my3d.config.filter.JwtTokenFilter;
+import joo.project.my3d.config.handler.CustomOAuth2SuccessHandler;
 import joo.project.my3d.domain.UserAccount;
 import joo.project.my3d.domain.constant.UserRole;
 import joo.project.my3d.dto.UserAccountDto;
+import joo.project.my3d.dto.properties.JwtProperties;
 import joo.project.my3d.dto.response.KakaoOauth2Response;
 import joo.project.my3d.dto.security.BoardPrincipal;
 import joo.project.my3d.service.UserAccountService;
@@ -13,15 +17,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.util.UUID;
 
@@ -43,7 +47,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
-            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService,
+            UserAccountService userAccountService,
+            JwtProperties jwtProperties,
+            CustomOAuth2SuccessHandler customOAuth2SuccessHandler
     ) throws Exception {
         return http
                 .csrf().disable()
@@ -77,24 +84,26 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling()
+//                .and()
+//                    .sessionManagement()
+//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션이 아닌 JWT를 이용하여 인증 진행
                 .and()
-                    .formLogin(form -> form
-                            .loginPage("/account/login")
-                            .usernameParameter("email")
-                    )
-                    .logout(logout -> logout
-                            .logoutUrl("/account/logout")
-                            .logoutSuccessUrl("/")
-                    )
+                    .formLogin().disable()
                     .oauth2Login(oAuth -> oAuth
                             .loginPage("/account/login")
                             .userInfoEndpoint(userInfo -> userInfo
                                     .userService(oAuth2UserService))
                             .defaultSuccessUrl("/account/type", true)
+                            .successHandler(customOAuth2SuccessHandler) //OAuth 로그인 후 cookie에 jwt 토큰 저장
                     )
+                    //cookie에서 token을 가져와 authentication 등록
+                    .addFilterBefore(
+                            new JwtTokenFilter(userAccountService, jwtProperties),
+                            BasicAuthenticationFilter.class)
+                    //회원가입이 안된 유저는 authentication을 제거하고 세션에 정보를 저장
                     .addFilterAfter(
                             new CustomOAuth2LoginAuthenticationFilter(),
-                            OAuth2LoginAuthenticationFilter.class
+                            UsernamePasswordAuthenticationFilter.class
                     )
                 .build();
     }
@@ -134,14 +143,11 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(UserAccountService userAccountService) {
         //loadUserByUsername
-        return email -> userAccountService
-                .searchUser(email)
-                .map(BoardPrincipal::from)
-                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - email: " + email));
+        return userAccountService::getUserPrincipal;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public BCryptPasswordEncoder encoderPassword() {
+        return new BCryptPasswordEncoder();
     }
 }
