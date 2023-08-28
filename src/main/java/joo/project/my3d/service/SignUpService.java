99b4e6426760283c9994d84bb9b5609576dc401a
JadeKim042386 <jk042386@gmail.com>
2023-08-28 19:56:19 +0900

    feat: 사업자 인증 API 비동기 처리 - ajax를 사용하여 API 통신을 통해 새로고침없이 인증을 진행하도록함 - 사업자 등록번호 형식과 에러 메시지도 자바스크립트로 처리하도록 수정 - ajax 통신 중에는 마우스 커서를 로딩 상태로 바꾸도록 적용 (응답이 빨라서 불필요해보이기도함) - 자바스크립트로 모두 진행하기 때문에 사업자 인증관련 컨트롤러와 비지니스 로직을 삭제하고 관련 테스트도 삭제 - 사업자 인증 후 회원가입 페이지의 URL이 파라미터와 같이 노출되면 인증이 안되어도 접근할 수 있으므로 숨김 처리 - 웹 페이지 소스에서도 자바스크립트 또는 href가 노출되지 않도록 추후 처리해야할예정 #38

--- src/main/java/joo/project/my3d/service/SignUpService.java
+++ src/main/java/joo/project/my3d/service/SignUpService.java
@@ -1,53 +1,15 @@
 package joo.project.my3d.service;
 
 import joo.project.my3d.dto.UserAccountDto;
-import joo.project.my3d.dto.response.BusinessCertificationApiResponse;
 import joo.project.my3d.dto.security.BoardPrincipal;
 import lombok.RequiredArgsConstructor;
-import net.minidev.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpService {
-
-    private final RestTemplate restTemplate;
-
-    @Value("${nts.service-key}")
-    private String serviceKey;
-    private final String NtsApiUrl =  "https://api.odcloud.kr/api/nts-businessman/v1/status";
-
-    /**
-     * 국세청 API를 활용한 사업자 인증 로직<br>
-     * b_stt (01: 계속사업자, 02: 휴업자, 03: 폐업자)
-     */
-    public String businessCertification(String b_no) {
-        URI uri = UriComponentsBuilder
-                .fromHttpUrl(NtsApiUrl)
-                .queryParam("serviceKey", serviceKey)
-                .build()
-                .toUri();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        JSONObject body = new JSONObject();
-        body.put("b_no", new String[]{b_no});
-
-        HttpEntity<Object> entity = new HttpEntity<>(body, headers);
-        ResponseEntity<BusinessCertificationApiResponse> response = restTemplate.exchange(uri, HttpMethod.POST, entity, BusinessCertificationApiResponse.class);
-
-        return Optional.ofNullable(response.getBody())
-                .orElseGet(BusinessCertificationApiResponse::empty).data().get(0).b_stt_cd();
-    }
 
     /**
      * 직접 authentication 등록
