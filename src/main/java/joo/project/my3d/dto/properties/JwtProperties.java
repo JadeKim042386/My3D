0f18d69f0ceb5fd0b5ca696034ad238559f516a0
JadeKim042386 <jk042386@gmail.com>
2024-01-13 17:53:33 +0900

    feat: access token과 refresh token의 expired time 설정 - accesss token은 30분, refresh token은 24시간으로 설정 #188

--- src/main/java/joo/project/my3d/dto/properties/JwtProperties.java
+++ src/main/java/joo/project/my3d/dto/properties/JwtProperties.java
@@ -5,11 +5,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * @param cookieName 쿠키에 저장될때 사용되는 키 값
  * @param secretKey 토큰을 생성할때 사용되는 키 값
- * @param expiredTimeMs 토큰 만료 시간 (ms)
+ * @param accessExpiredMs access 토큰 만료 시간 (ms)
+*  @param refreshExpiredMs refresh 토큰 만료 시간 (ms)
  */
-@ConfigurationProperties("jwt.token")
+@ConfigurationProperties(prefix = "jwt.token")
 public record JwtProperties(
         String cookieName,
         String secretKey,
-        long expiredTimeMs
+        long accessExpiredMs,
+        long refreshExpiredMs
 ) {}
