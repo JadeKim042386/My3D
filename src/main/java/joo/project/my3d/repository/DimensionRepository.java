76896162fa550cdb6ce1d0c2194252693c98620c
JadeKim042386 <jk042386@gmail.com>
2024-01-16 21:04:48 +0900

    refactor: Dimension과 DimensionOption의 비지니스 로직은 사용되지 않으므로 삭제

--- src/main/java/joo/project/my3d/repository/DimensionRepository.java
+++ src/main/java/joo/project/my3d/repository/DimensionRepository.java
@@ -3,8 +3,5 @@ package joo.project.my3d.repository;
 import joo.project.my3d.domain.Dimension;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DimensionRepository extends JpaRepository<Dimension, Long> {
-    List<Dimension> findAllByDimensionOptionId(Long dimensionOptionId);
 }
